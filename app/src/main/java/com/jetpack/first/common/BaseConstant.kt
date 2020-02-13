326dcf4db331433af8a6164e406f0fa998939d9c
LiuHuiAndroid <642404044@qq.com>
2020-02-13 23:25:00 +0800

    Data Binding

---
+++ app/src/main/java/com/jetpack/first/common/BaseConstant.kt
@@ -0,0 +1,8 @@
+package com.jetpack.first.common
+
+object BaseConstant {
+
+    const val USER_NAME:String = "admin"
+    const val USER_PWD:String = "123456"
+
+}
--- app/src/main/java/com/jetpack/first/ui/fragment/login/LoginFragment.kt
+++ app/src/main/java/com/jetpack/first/ui/fragment/login/LoginFragment.kt
@@ -4,20 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.jetpack.first.MainActivity
 import com.jetpack.first.R
+import com.jetpack.first.databinding.FragmentLoginBinding
+import com.jetpack.first.viewmodel.LoginModel
 import kotlinx.android.synthetic.main.fragment_login.*
 import org.jetbrains.anko.support.v4.startActivity
 
 class LoginFragment : Fragment() {
+
+    private lateinit var loginModel: LoginModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(
+            inflater,
+            R.layout.fragment_login,
+            container,
+            false
+        )
+        loginModel = LoginModel("", "", context!!)
+        binding.model = loginModel
+        binding.activity = activity
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
